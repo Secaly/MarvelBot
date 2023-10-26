4cff7f10ed5104f8b7a00e4f0c3a974fe170b19e
Secaly <kevin.ledieu.fr@gmail.com>
2023-10-26 16:08:11 +0200

    Updating handlers logic

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,10 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import * as dotenv from 'dotenv';
 import { loadHandlers } from './functions/library/launch.js';
+import type { ExecutableButtonComponent, ExecutableSlashCommand } from './types.js';
 
 // Load environment variables from .env
 dotenv.config();
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-});
 
 // Access Discord token from the environment variables
 const token = process.env.DISCORD_TOKEN;
@@ -16,6 +13,18 @@ if (!token) {
   console.error('Discord token is missing in the .env file.');
   process.exit(1);
 }
+
+// Setup the Discord Client
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+// Commands collections
+client.executableSlashCommands = new Collection<string, ExecutableSlashCommand>();
+// Components collections
+client.executableButtonComponents = new Collection<string, ExecutableButtonComponent>();
+
 await loadHandlers(client);
 
 client.login(token);
